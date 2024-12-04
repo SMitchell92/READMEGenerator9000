0b8f7d11f4b894aad83f8764f3d3a2c6a2c2991d
StuMitch <stuart.mitchell92@gmail.com>
2024-12-04 15:41:23 -0800

    added README file template, created functions to properly generate readme file, created array of objects for questions and set it up to insert the answers into the generated readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,78 @@
-// TODO: Include packages needed for this application
-
+ // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown';
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: 'Title',
+    message: `What is the Title of your application?`
+},
+{
+    type: "input",
+    name: 'Description',
+    message: `What is the Description of your app?`
+},
+{
+    type: "input",
+    name: "Installation",
+    message: `How do you install your app?`
+},
+{
+    type: "input",
+    name: "Usage",
+    message: `What is the app used for?`
+},
+{
+    type: "list",
+    name: "License",
+    message: `What license did you use for your app?`,
+    choices: ["MIT", 'Aache License 2.0', 'GNU General Public License (GPL) v3', 'BSD 3-Clause License', 'Mozilla Public LIcense 2.0', 'No License'],
+},
+{
+    type: "input",
+    name: "Contributing",
+    message: `How do others contribute to the app?`
+},
+{
+    type: "input",
+    name: "Tests",
+    message: `What are some tests for your app?`
+},
+{
+    type: "input",
+    name: "gitHubUsername",
+    message: `What is your GitHub Username?`
+},
+{
+    type: "input",
+    name: "email",
+    message: `What is your email?`
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        if (err) {
+            console.log("there was an err")
+        }
+        else {
+            console.log("README Successfully Written")
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(
+            questions
+        ).then(function (answers) {
+            const markdown = generateMarkdown(answers)
+            writeToFile("README.md", markdown)
+        })
+    // fs.appendFile(README.txt,)
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+import fs from "fs";
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
 
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge()}
+## Description
 
-`;
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${answers.Installation}
+
+## Usage
+
+${answers.Usage}
+
+## License
+
+FIX HERE
+---
+
+## Contributing
+
+${answers.Contributing}
+
+## Tests
+
+${answers.Tests}
+
+## Questions
+
+For any further questions, please contact me via one of the following methods:
+GitHub Account: ${answers.gitHubUsername}
+Email Address: ${answers.email}
+`
 }
 
 export default generateMarkdown;
